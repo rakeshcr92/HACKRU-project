ba8e8d6096c50a3cc5a4e1ed2e23f2eae7b1495b
rakeshcr92 <160077191+rakeshcr92@users.noreply.github.com>
2024-03-24 08:02:43 -0400

    Add files via upload

---
+++ website/__init__.py
@@ -0,0 +1,33 @@
+﻿from flask import Flask
+
+
+
+
+
+
+
+def create_app():
+    app = Flask(__name__)
+    
+
+    from .views import views
+    from .prediction import prediction
+    
+    
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(prediction, url_prefix='/')
+    
+
+    
+  
+
+    
+
+
+
+    return app
+
+
+
+
