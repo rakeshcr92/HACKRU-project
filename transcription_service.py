437273547cc34d736412f745bc75a77b9718948a
rakeshcr92 <160077191+rakeshcr92@users.noreply.github.com>
2024-03-24 11:50:39 -0400

    Update transcription_service.py

--- transcription_service.py
+++ transcription_service.py
@@ -1,11 +1,10 @@
-﻿# transcription_service.py
+# transcription_service.py
 
 import whisper
 
-
 class TranscriptionService:
     def transcribe(self, link, model_name, api_key):
-        # Transcribe audio using Whisper.
+        # Utilize Whisper for audio transcription.
         model = whisper.load_model(model_name)
         result = model.transcribe(audio=link, verbose=True)
         return result["text"]
